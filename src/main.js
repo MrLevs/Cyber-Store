519eb061d754b69e68169422c4004804566a17fe
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2025-03-04 22:53:52 +0300

    index.html header sass

--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
 'use strict';
 import './sass/style.scss';
+
+const slider = document.querySelector('#slider-theme');
+slider.addEventListener('change', toggleTheme);
+
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.body.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light');
+  } else {
+    setTheme('theme-dark');
+  }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+    document.getElementById('slider-theme').checked = false;
+  } else {
+    setTheme('theme-light');
+    document.getElementById('slider-theme').checked = true;
+  }
+})();
