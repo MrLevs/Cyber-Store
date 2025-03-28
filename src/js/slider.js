14ae9020953a8b99f0d90f350981ccf65e1919ab
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2025-03-28 22:50:45 +0300

    slider

---
+++ src/js/slider.js
@@ -0,0 +1,51 @@
+'use strict';
+
+export default function () {
+  const slider = document.querySelector('.slider');
+  const sliderContainer = document.querySelector('.slider__container');
+  const slides = document.querySelectorAll('.slider__slide');
+
+  const btnPrev = document.querySelector('.slider__btn-prev');
+  const btnNext = document.querySelector('.slider__btn-next');
+
+  let count = 0;
+  let width;
+
+  function init() {
+    console.log('resize');
+    width = slider.offsetWidth;
+    // sliderContainer.style.width = width * slides.length + 'px';
+    // slides.forEach(item => {
+    //   item.style.width = width + 'px';
+    //   item.style.height = 'auto';
+    // });
+    // rollSlider();
+    console.log(width);
+  }
+  window.addEventListener('resize', init);
+  init();
+
+  btnPrev.addEventListener('click', movePrev);
+  btnNext.addEventListener('click', moveNext);
+
+  function movePrev() {
+    count--;
+    if (count < 0) {
+      count = slides.length - 1;
+    }
+    rollSlider();
+  }
+
+  function moveNext() {
+    count++;
+    if (count >= slides.length) {
+      count = 0;
+    }
+    console.log(count);
+    rollSlider();
+  }
+
+  function rollSlider() {
+    sliderContainer.style.transform = 'translate(-' + count * width + 'px)';
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import './sass/style.scss';
 //Slider Theme
 import theme from './js/theme.js';
 import burgerMenu from './js/burger-menu.js';
+import slider from './js/slider.js';
 
 //Slider Theme
 theme();
 
 //Burger Menu
 burgerMenu();
+
+//Slider
+slider();
