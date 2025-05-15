41b3cd1b745b3159d9adf9cd3431ea576121b525
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2025-05-15 22:18:20 +0300

    search.js create svg and fix valid()

--- src/js/search.js
+++ src/js/search.js
@@ -4,7 +4,7 @@ export default function () {
   const forms = document.querySelectorAll('.form-search');
 
   const searchResults = [];
-  const data = ['mobile', 'air', 'ipad', 'iphone', 'macbook', 'macmini', 'imac'];
+  const data = ['mobile', 'air', 'ipad', 'iphone', 'macbook', 'macmini', 'imac', 'iphone 16', 'macbook 16 pro max'];
 
   forms.forEach(item => {
     const input = item.querySelector('.form-search__input');
@@ -12,6 +12,7 @@ export default function () {
     const resultSuggests = item.querySelector('.form-search__suggests');
 
     resultSuggests.style.display = 'none';
+    btn.style.display = 'none';
 
     input.addEventListener('input', function () {
       let value = input.value.toLowerCase().trim();
@@ -19,29 +20,30 @@ export default function () {
       resultSuggests.style.display = 'block';
       searchResults.splice(0, searchResults.length);
 
-      if (valid(value)) {
+      if (valid(input.value)) {
+        btn.style.display = 'block';
         if (value !== '') {
-          btn.style.display = 'block';
           data.forEach(item => {
             if (item.includes(value)) {
               searchResults.push(item);
             }
           });
+          displayResults(resultSuggests, searchResults);
+        }
+      } else {
+        if (input.value !== '') {
+          btn.style.display = 'block';
         } else {
-          resultSuggests.innerHTML = '';
-          resultSuggests.style.display = 'none';
           btn.style.display = 'none';
         }
-
-        displayResults(resultSuggests, searchResults);
-      } else {
-        if (value == ' ') {
-          console.log(1111);
-        }
-        console.log('error');
         resultSuggests.innerHTML = '';
         resultSuggests.style.display = 'none';
-        btn.style.display = 'none';
+        input.addEventListener('keydown', e => {
+          if (e.code === 'Enter') {
+            e.preventDefault();
+            alert('Поле ввода должно содержать только буквы и цифры!!!');
+          }
+        });
       }
     });
 
@@ -53,7 +55,7 @@ export default function () {
   });
 
   function valid(elem) {
-    const pattern = /^[a-zA-Zа-яА-яёЁ0-9\s]+$/;
+    const pattern = /^[\sa-zа-я0-9]+$/i;
     return pattern.test(elem);
   }
 
@@ -67,11 +69,37 @@ export default function () {
       if (index <= 9) {
         const li = document.createElement('li');
         const a = document.createElement('a');
+        const div = document.createElement('div');
+        const span = document.createElement('span');
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+        li.className = 'form-search__suggests-item';
 
         a.className = 'form-search__link';
         a.setAttribute('href', '#');
-        a.textContent = item;
 
+        div.className = 'form-search__suggests-box';
+        span.className = 'form-search__suggests-text';
+        span.textContent = item;
+
+        svg.setAttribute('width', '18');
+        svg.setAttribute('height', '18');
+        svg.setAttribute('viewBox', '0 0 18 18');
+        svg.setAttribute('fill', 'none');
+        svg.setAttribute('class', 'form-search__suggests-svg');
+
+        path.setAttribute(
+          'd',
+          'M16.9331 17L13.1554 13.2156M15.2489 8.15789C15.2489 10.0563 14.4948 11.8769 13.1524 13.2193C11.81 14.5617 9.98939 15.3158 8.091 15.3158C6.19261 15.3158 4.37197 14.5617 3.0296 13.2193C1.68724 11.8769 0.933105 10.0563 0.933105 8.15789C0.933105 6.2595 1.68724 4.43886 3.0296 3.0965C4.37197 1.75413 6.19261 1 8.091 1C9.98939 1 11.81 1.75413 13.1524 3.0965C14.4948 4.43886 15.2489 6.2595 15.2489 8.15789V8.15789Z',
+        );
+        path.setAttribute('stroke-width', '1.5');
+        path.setAttribute('stroke-linecap', 'round');
+
+        svg.append(path);
+        div.append(svg);
+        div.append(span);
+        a.append(div);
         li.append(a);
         ul.append(li);
         elem.append(ul);
@@ -81,3 +109,4 @@ export default function () {
     });
   }
 }
+// aria-selected li
