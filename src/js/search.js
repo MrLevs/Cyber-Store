3c0a38b1586feea36ca054c0b717f1783926f9f8
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2025-05-28 01:02:18 +0300

    fix search.js, loop  for

--- src/js/search.js
+++ src/js/search.js
@@ -3,6 +3,9 @@
 export default function () {
   const forms = document.querySelectorAll('.form-search');
 
+  let searchItems;
+  let count = 0;
+
   const searchResults = [];
   const data = [
     {
@@ -39,6 +42,38 @@ export default function () {
     },
     {
       name: 'macbook 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook21 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook22 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook23 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook24 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook25 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook26 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook27 16 max',
+      link: '#macbook16max',
+    },
+    {
+      name: 'macbook28 16 max',
       link: '#macbook16max',
     },
   ];
@@ -78,6 +113,7 @@ export default function () {
       let value = input.value.toLowerCase().trim();
 
       input.removeEventListener('keydown', cancel);
+      input.removeEventListener('keydown', stepArrow);
       input.classList.remove('form-search__input_warning');
 
       resultSuggests.classList.remove('form-search__suggests_warning');
@@ -91,16 +127,21 @@ export default function () {
 
         if (value == '') {
           input.addEventListener('keydown', cancel);
-        }
-
-        if (valid(input.value)) {
+        } else if (valid(input.value)) {
           if (value !== '') {
-            data.forEach(item => {
-              if (item.name.includes(value)) {
-                searchResults.push(item);
+            for (let i = 0; i < data.length; i++) {
+              if (data[i].name.includes(value)) {
+                if (searchResults.length < 10) {
+                  searchResults.push(data[i]);
+                } else {
+                  console.log(searchResults);
+                  break;
+                }
               }
-            });
+            }
+
             displayResults(resultSuggests, searchResults);
+            input.addEventListener('keydown', stepArrow);
           }
         } else {
           resultSuggests.innerHTML = '';
@@ -129,54 +170,112 @@ export default function () {
     return pattern.test(elem);
   }
 
+  //--------at work-----
+  function stepArrow(e) {
+    if (e.code === 'ArrowUp') {
+      e.preventDefault();
+      count--;
+
+      if (count <= 0) {
+        count = searchItems.length;
+      }
+
+      searchItems.forEach(item => {
+        item.classList.remove('form-search__suggests-item_active');
+        item.setAttribute('aria-selected', false);
+
+        if (count == item.dataset.suggestsIndex) {
+          item.classList.add('form-search__suggests-item_active');
+          item.setAttribute('aria-selected', true);
+        }
+      });
+    } else if (e.code === 'ArrowDown') {
+      e.preventDefault();
+      count++;
+      if (count > searchItems.length) {
+        count = 1;
+      }
+
+      searchItems.forEach(item => {
+        item.classList.remove('form-search__suggests-item_active');
+        item.setAttribute('aria-selected', false);
+        if (count == item.dataset.suggestsIndex) {
+          item.classList.add('form-search__suggests-item_active');
+          item.setAttribute('aria-selected', true);
+        }
+      });
+    } else if (e.code === 'Enter') {
+      e.preventDefault();
+      let linkHref;
+
+      for (let i = 0; i < searchItems.length; i++) {
+        if (searchItems[i].getAttribute('aria-selected') == 'true') {
+          linkHref = searchItems[i].querySelector('.form-search__link').href;
+          break;
+        }
+      }
+
+      if (linkHref) {
+        console.log(linkHref);
+      } else {
+        console.log('STOP');
+      }
+    }
+  }
+  //---------------------------------------
+
   function displayResults(elem, array) {
     elem.innerHTML = '';
+    count = 0;
 
     const ul = document.createElement('ul');
     ul.className = 'form-search__suggests-list';
+    ul.setAttribute('role', 'listbox');
+    ul.setAttribute('aria-labelledby', 'lable-search');
 
     array.forEach((item, index) => {
-      if (index <= 9) {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        const div = document.createElement('div');
-        const span = document.createElement('span');
-        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      const div = document.createElement('div');
+      const span = document.createElement('span');
+      const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
-        li.className = 'form-search__suggests-item';
+      li.className = 'form-search__suggests-item';
+      li.dataset.suggestsIndex = index + 1;
+      li.setAttribute('role', 'option');
+      li.setAttribute('aria-selected', false);
 
-        a.className = 'form-search__link';
-        a.setAttribute('href', `${item.link}`);
+      a.className = 'form-search__link';
+      a.setAttribute('href', `${item.link}`);
 
-        div.className = 'form-search__suggests-box';
-        span.className = 'form-search__suggests-text';
-        span.textContent = item.name;
+      div.className = 'form-search__suggests-box';
+      span.className = 'form-search__suggests-text';
+      span.textContent = item.name;
 
-        svg.setAttribute('width', '18');
-        svg.setAttribute('height', '18');
-        svg.setAttribute('viewBox', '0 0 18 18');
-        svg.setAttribute('fill', 'none');
-        svg.setAttribute('class', 'form-search__suggests-svg');
+      svg.setAttribute('width', '18');
+      svg.setAttribute('height', '18');
+      svg.setAttribute('viewBox', '0 0 18 18');
+      svg.setAttribute('fill', 'none');
+      svg.setAttribute('class', 'form-search__suggests-svg');
 
-        path.setAttribute(
-          'd',
-          'M16.9331 17L13.1554 13.2156M15.2489 8.15789C15.2489 10.0563 14.4948 11.8769 13.1524 13.2193C11.81 14.5617 9.98939 15.3158 8.091 15.3158C6.19261 15.3158 4.37197 14.5617 3.0296 13.2193C1.68724 11.8769 0.933105 10.0563 0.933105 8.15789C0.933105 6.2595 1.68724 4.43886 3.0296 3.0965C4.37197 1.75413 6.19261 1 8.091 1C9.98939 1 11.81 1.75413 13.1524 3.0965C14.4948 4.43886 15.2489 6.2595 15.2489 8.15789V8.15789Z',
-        );
-        path.setAttribute('stroke-width', '1.5');
-        path.setAttribute('stroke-linecap', 'round');
+      path.setAttribute(
+        'd',
+        'M16.9331 17L13.1554 13.2156M15.2489 8.15789C15.2489 10.0563 14.4948 11.8769 13.1524 13.2193C11.81 14.5617 9.98939 15.3158 8.091 15.3158C6.19261 15.3158 4.37197 14.5617 3.0296 13.2193C1.68724 11.8769 0.933105 10.0563 0.933105 8.15789C0.933105 6.2595 1.68724 4.43886 3.0296 3.0965C4.37197 1.75413 6.19261 1 8.091 1C9.98939 1 11.81 1.75413 13.1524 3.0965C14.4948 4.43886 15.2489 6.2595 15.2489 8.15789V8.15789Z',
+      );
+      path.setAttribute('stroke-width', '1.5');
+      path.setAttribute('stroke-linecap', 'round');
 
-        svg.append(path);
-        div.append(svg);
-        div.append(span);
-        a.append(div);
-        li.append(a);
-        ul.append(li);
-        elem.append(ul);
-      } else {
-        return;
-      }
+      svg.append(path);
+      div.append(svg);
+      div.append(span);
+      a.append(div);
+      li.append(a);
+      ul.append(li);
+      elem.append(ul);
     });
+    searchItems = document.querySelectorAll('.form-search__suggests-item');
   }
 }
-// aria-selected li
+
+// Остановка цикла, for || forEach
