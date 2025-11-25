04c0b1bb766645c4b230a653803dfd1497462c48
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2025-11-25 23:18:26 +0300

    +data in data.json

--- src/js/data-base.js
+++ src/js/data-base.js
@@ -4,13 +4,7 @@ export default async function () {
   try {
     let response = await fetch('/data.json');
     let result = await response.json();
-    let data = result.map(({ title, name, link, price }) => ({
-      title: title.toLowerCase(),
-      name: name.toLowerCase(),
-      link: link,
-      price: price,
-    }));
-    return data;
+    return result;
   } catch (err) {
     alert(err + ' ' + 'Data base not working!');
     console.log(err + ' ' + 'Data base not working!');
