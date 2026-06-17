ec4341c86114d1c994f5ad9bc78c202291934a52
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2026-06-18 00:45:42 +0300

    create slider-payment.js

--- src/js/slider-payment.js
+++ src/js/slider-payment.js
@@ -2,19 +2,21 @@
 
 export function sliderPayment() {
   const paymentSteps = document.querySelectorAll('.payment__step');
+  const paymentOrder = document.querySelector('.payment__order');
   const paymentSlides = document.querySelectorAll('.payment__info-box');
   const btnBack = document.querySelector('#btn-back');
   const btnNext = document.querySelector('#btn-next');
   const btnPay = document.querySelector('#btn-pay');
+  let currentIndex = 0;
 
   paymentSteps.forEach(item => {
-    if (item.dataset.step === 'address') {
+    if (item.dataset.step === currentIndex.toString()) {
       item.classList.add('payment__step_active');
     }
   });
 
-  paymentSlides.forEach(item => {
-    if (item.dataset.payment !== 'address') {
+  paymentSlides.forEach((item, index) => {
+    if (index !== currentIndex) {
       item.classList.add('payment__info-box_disabled');
     }
   });
@@ -28,36 +30,52 @@ export function sliderPayment() {
 
   function stepBack(event) {
     event.preventDefault();
+    if (currentIndex !== 0) {
+      paymentSlides[currentIndex].classList.add('payment__info-box_disabled');
+    }
+    currentIndex--;
+
+    if (currentIndex < 0) {
+      currentIndex = 0;
+    }
+
+    paymentSlides[currentIndex].classList.remove('payment__info-box_disabled');
+
+    toggleSteps();
   }
 
   function stepNext(event) {
     event.preventDefault();
-    for (let i = 0; i < paymentSlides.length; i++) {
-      if (paymentSlides[i].classList.contains('payment__info-box_disabled')) {
-        switch (paymentSlides[i].dataset.payment) {
-          case 'address':
-            paymentSlides.forEach(item => {
-              if (item.classList.contains('payment__info-box_disabled')) {
-                item.classList.remove('payment__info-box_disabled');
-              }
-              if (item.dataset.payment !== 'shipping') {
-                item.classList.add('payment__info-box_disabled');
-              }
-            });
-            break;
-          case 'shipping':
-            paymentSlides.forEach(item => {
-              if (item.classList.contains('payment__info-box_disabled')) {
-                item.classList.remove('payment__info-box_disabled');
-              }
-              if (item.dataset.payment !== 'pay') {
-                item.classList.add('payment__info-box_disabled');
-              }
-            });
-            break;
-        }
+    if (currentIndex !== paymentSlides.length - 1) {
+      paymentSlides[currentIndex].classList.add('payment__info-box_disabled');
+    }
+    currentIndex++;
+
+    if (currentIndex < paymentSlides.length) {
+      paymentSlides[currentIndex].classList.remove('payment__info-box_disabled');
+    } else {
+      currentIndex = paymentSlides.length - 1;
+    }
+
+    toggleSteps();
+  }
+
+  function toggleSteps() {
+    paymentSteps.forEach(item => {
+      if (item.classList.contains('payment__step_active')) {
+        item.classList.remove('payment__step_active');
       }
-      break;
+      if (item.dataset.step === currentIndex.toString()) {
+        item.classList.add('payment__step_active');
+      }
+    });
+
+    if (currentIndex === paymentSlides.length - 1) {
+      paymentOrder.classList.remove('payment__order_disabled');
+    } else {
+      if (!paymentOrder.classList.contains('payment__order_disabled')) {
+        paymentOrder.classList.add('payment__order_disabled');
+      }
     }
   }
 }
