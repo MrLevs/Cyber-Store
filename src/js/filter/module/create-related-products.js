37dd09335811a00224dbe547b13926b3da25308a
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2026-06-24 21:47:04 +0300

    fix createRelatedProduct.js

--- src/js/filter/module/create-related-products.js
+++ src/js/filter/module/create-related-products.js
@@ -6,13 +6,13 @@ export function createRelatedProducts(data, like) {
   const relatedBlock = document.querySelector('#related');
   let relatedProducts = [];
 
-  data.forEach(item => {
-    if (item.brand === 'apple') {
-      if (relatedProducts.length < 4) {
-        relatedProducts.push(item);
-      }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].brand === 'apple' && relatedProducts.length < 4) {
+      relatedProducts.push(data[i]);
+    } else {
+      break;
     }
-  });
+  }
 
   if (relatedBlock) {
     createProductCard(relatedBlock, relatedProducts, like);
