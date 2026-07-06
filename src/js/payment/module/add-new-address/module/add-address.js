7066858445479c3643466fdc6616dc2237cbabe6
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2026-07-06 20:49:59 +0300

    payment.scss dialog

--- src/js/payment/module/add-new-address/module/add-address.js
+++ src/js/payment/module/add-new-address/module/add-address.js
@@ -1,6 +1,26 @@
 'use strict';
 
 export function addAddress() {
-  const bangeAddress = document.querySelector('#form-add-address-bange').value;
-  console.log(bangeAddress);
+  let bangeAddress = document.querySelector('#form-add-address-bange').value.trim().toLowerCase();
+  let titleAddress = document.querySelector('#form-add-address-title').value.trim().toLowerCase();
+  let postcodeAddress = document.querySelector('#form-add-address-postcode').value.trim();
+  let cityAddress = document.querySelector('#form-add-address-city').value.trim().toLowerCase();
+  let streetAddress = document.querySelector('#form-add-address-street').value.trim().toLowerCase();
+  let apartmentAddress = document.querySelector('#form-add-address-apartment').value.trim();
+  let telephoneAddress = document.querySelector('#form-add-address-telephone').value.trim();
+
+  if (!apartmentAddress) {
+    apartmentAddress = 'not specified';
+  }
+
+  let userAddress = {
+    bange: bangeAddress,
+    title: titleAddress,
+    postcode: postcodeAddress,
+    city: cityAddress,
+    street: streetAddress,
+    apartment: apartmentAddress,
+    telephone: telephoneAddress,
+  };
+  console.log(userAddress);
 }
