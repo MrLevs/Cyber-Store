65e867bc369b50e922e32b4ac3f1b5006fc639cc
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2026-09-10 22:34:36 +0300

    send-comment.js complete

--- src/js/select-details-product/module/send-comment/module/get-comment.js
+++ src/js/select-details-product/module/send-comment/module/get-comment.js
@@ -1,3 +1,60 @@
 'use strict';
 
-export function getComment() {}
+import { showWarningInput } from '../../../../warning'; // Show Warning Input
+import { strUpCase } from '../../../../string-up-case'; // String UpperCase
+
+export function getComment() {
+  const message = 'Warning!!! The field must be filled in correctly and cannot be left blank!';
+  const inputName = document.querySelector('#form-review-name');
+  const inputSurname = document.querySelector('#form-review-surname');
+  const inputComment = document.querySelector('#form-review-comment');
+  const ratingWarning = document.querySelector('.form-review__rating-warning');
+  const file = document.querySelector('#form-review-file');
+  let rating = document.querySelector('input[name="stars"]:checked');
+  let name = inputName.value.trim();
+  let surname = inputSurname.value.trim();
+  let comment = inputComment.value.replace(/\s+/g, ' ').trim();
+  let userReview = {};
+
+  if (name === '' || inputName.classList.contains('warning__input')) {
+    showWarningInput(inputName, message);
+  } else {
+    userReview.name = strUpCase(name);
+  }
+
+  if (surname === '' || inputSurname.classList.contains('warning__input')) {
+    showWarningInput(inputSurname, message);
+  } else {
+    userReview.surname = strUpCase(surname);
+  }
+
+  if (rating) {
+    userReview.rating = rating.value;
+  } else {
+    let stars = document.querySelectorAll('.form-review__svg');
+    stars.forEach(item => {
+      item.classList.add('form-review__svg_warning');
+    });
+    ratingWarning.style.display = 'block';
+    ratingWarning.textContent = 'Rate the product!';
+  }
+
+  if (inputComment.classList.contains('warning__input')) {
+    showWarningInput(inputComment, message);
+  } else {
+    userReview.comment = comment;
+  }
+
+  if (file.files.length > 0) {
+    userReview.file = file.files;
+  }
+
+  if (
+    userReview.name !== undefined &&
+    userReview.surname !== undefined &&
+    userReview.rating !== undefined &&
+    !inputComment.classList.contains('warning__input')
+  ) {
+    return userReview;
+  }
+}
--- src/js/select-details-product/module/send-comment/send-comment.js
+++ src/js/select-details-product/module/send-comment/send-comment.js
@@ -1,26 +1,49 @@
 'use strict';
 
+import { getComment } from './module/get-comment'; // Get User Review
 import { validCommentInput } from './module/valid-comment-input'; // Validation Input
 
 export function sendComment() {
   const modalReview = document.querySelector('#dialog-review');
   const btnOpenModal = document.querySelector('.leave-comment');
   const btnCloseModal = document.querySelector('#close-dialog-review');
-  const inputComment = Array.from(document.querySelectorAll('.form-review__input'));
+  const inputRating = document.querySelectorAll('.form-review__radio');
+  const labelRating = document.querySelectorAll('.form-review__star');
+  const ratingWarning = document.querySelector('.form-review__rating-warning');
+  const inputComment = document.querySelectorAll('.form-review__input');
   const inputFile = document.querySelector('#form-review-file');
   const container = document.querySelector('.form-review__preview');
   const btnSendComment = document.querySelector('.form-review__btn');
   let urlFilePreview = [];
+  let userReview;
+
+  ratingWarning.style.display = 'none';
 
   if (btnOpenModal && btnCloseModal) {
     btnOpenModal.addEventListener('click', openModal);
     btnCloseModal.addEventListener('click', closeModal);
   }
 
-  if (inputComment) {
+  if (inputComment.length > 0) {
     inputComment.forEach(item => {
       item.addEventListener('input', () => {
         validCommentInput(item);
+      });
+    });
+  }
+
+  if (labelRating.length > 0) {
+    labelRating.forEach(item => {
+      item.addEventListener('click', () => {
+        let stars = document.querySelectorAll('.form-review__svg');
+        stars.forEach(item => {
+          item.classList.remove('form-review__svg_warning');
+        });
+
+        if (ratingWarning.textContent.trim() !== '') {
+          ratingWarning.innerHTML = '';
+          ratingWarning.style.display = 'none';
+        }
       });
     });
   }
@@ -45,8 +68,13 @@ export function sendComment() {
 
   if (btnSendComment) {
     btnSendComment.addEventListener('click', () => {
-      if (urlFilePreview.length > 0) {
-        clearUrl();
+      reviewSetDatabase();
+    });
+
+    btnSendComment.addEventListener('keydown', event => {
+      if (event.code === 'Enter') {
+        event.preventDefault();
+        reviewSetDatabase();
       }
     });
   }
@@ -59,10 +87,53 @@ export function sendComment() {
 
   function closeModal(event) {
     event.preventDefault();
+
+    const inputWarning = document.querySelectorAll('.warning__input');
+    const blockWarning = document.querySelectorAll('.warning_alert');
+
+    if (inputWarning.length > 0) {
+      inputWarning.forEach(item => {
+        item.classList.remove('warning__input');
+        item.value = '';
+      });
+    }
+
+    if (blockWarning.length > 0) {
+      blockWarning.forEach(item => {
+        item.innerHTML = '';
+        item.classList.remove('warning_alert');
+        item.style.display = 'none';
+      });
+    }
+    clearModalReview();
+  }
+
+  //------Review Set Database------------
+  function reviewSetDatabase() {
+    userReview = getComment();
+    if (userReview) {
+      let reviewsCollection = JSON.parse(localStorage.getItem('reviews'));
+      if (reviewsCollection) {
+        reviewsCollection.push(userReview);
+        localStorage.setItem('reviews', JSON.stringify(reviewsCollection));
+      } else {
+        localStorage.setItem('reviews', JSON.stringify([userReview]));
+      }
+      clearModalReview();
+    } else {
+      btnSendComment.blur();
+    }
+  }
+
+  //---------Clear ModalReview----------------------
+  function clearModalReview() {
     document.body.classList.remove('_lock');
     modalReview.close();
     inputComment.forEach(item => {
       item.value = '';
+    });
+    inputRating.forEach(item => {
+      item.checked = false;
     });
     container.innerHTML = '';
     if (urlFilePreview.length > 0) {
